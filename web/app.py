681932791b5ca52daeb5436875424b1594643d2d
mat-prog310 <matthias.duprey1@gmail.com>
2026-05-24 21:33:44 +0200

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 import os, sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -8,6 +9,9 @@ from config import Config
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SECRET_KEY'] = Config.SECRET_KEY
 app.config['DEBUG'] = False
+
+# CORS pour tous les domaines
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
