47ba6bac018f680d609a4fcf6ca78d14bca560cd
mat-prog310 <matthias.duprey1@gmail.com>
2026-05-24 11:28:13 +0200

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_socketio import SocketIO
 import os, sys
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import Config
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SECRET_KEY'] = Config.SECRET_KEY
 app.config['DEBUG'] = False
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+
+# Mode async compatible Render
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 from web import routes, socket_events
