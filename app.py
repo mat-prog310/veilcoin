ef85009976ae64ead39909ac0f1b7036788a106d
mat-prog310 <matthias.duprey1@gmail.com>
2026-05-24 11:48:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from web.app import app as application
-from web.app import socketio
+from web.app import app, socketio
 import os
 from config import Config
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', Config.API_PORT))
-    socketio.run(application, host='0.0.0.0', port=port, debug=False)
+    print(f"VeilCoin demarre sur le port {port}")
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
