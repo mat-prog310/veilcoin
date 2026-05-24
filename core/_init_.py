95c2c845070c0625f615acd99fde8593353c6416
mat-prog310 <matthias.duprey1@gmail.com>
2026-05-24 10:38:49 +0200

    Add LiquidityPool import to core module

--- core/_init_.py
+++ core/_init_.py
@@ -1,9 +1,7 @@
-"""
-VeilCoin Core - Module principal
-"""
 from .blockchain import Blockchain
 from .wallet import VeilWallet
 from .transaction import VeilTransaction, TransactionInput, TransactionOutput
 from .block import Block, BlockHeader
 from .consensus import Consensus
 from .randomx_miner import RandomXMiner
+from .payment import LiquidityPool
