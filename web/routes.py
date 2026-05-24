e562c75bb6d15e1ae000409b028646b8dfe2edad
mat-prog310 <matthias.duprey1@gmail.com>
2026-05-24 12:00:30 +0200

    Update routes.py

--- web/routes.py
+++ web/routes.py
@@ -132,3 +132,10 @@ def api_buy_offers(): return jsonify(pool.get_open_buy_offers())
 
 @app.route('/api/pool/info')
 def api_pool_info(): return jsonify(pool.get_pool_info())
+
+@app.after_request
+def add_cors(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
